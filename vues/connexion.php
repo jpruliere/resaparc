200aee58f14cdd2923ec7a8b894fc08c2073b13c
Vincent <julienbraga@hotmail.fr>
2018-05-23 14:34:39 +0200

    teste

--- vues/connexion.php
+++ vues/connexion.php
@@ -3,7 +3,6 @@
 <form action="../controleurs/controlBillet.php" method="get">
 
 	Votre numéro de billet : <input type="text" name="billet"><br>
-
 	<input type="submit" value="Connexion">
 
 </form>
